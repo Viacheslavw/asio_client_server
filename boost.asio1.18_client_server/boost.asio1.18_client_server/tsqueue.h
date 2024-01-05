edf6cffdf22b189632702e115280e5b2929791e3
Viacheslav <77392805+Viacheslavw@users.noreply.github.com>
2024-01-05 16:20:46 +0200

    add olc namespace

--- boost.asio1.18_client_server/boost.asio1.18_client_server/tsqueue.h
+++ boost.asio1.18_client_server/boost.asio1.18_client_server/tsqueue.h
@@ -35,7 +35,54 @@ namespace olc
 
 			T pop_back() {
 				std::scoped_lock lock(maxQueue);
+				auto t = std::move(deqQueue.back());
+				deqQueue.pop_back();
+				return t;
 			}
+
+			void push_back(const T& item) {
+				std::scoped_lock lock(maxQueue);
+				deqQueue.emplace_back(std::move(item));
+
+				std::unique_lock<std::mutex> ul(muxBlocking);
+				cvBlocking.notify_one();
+			}
+
+			void push_front(const T& item) {
+				std::scoped_lock lock(muxQueue);
+				deqQueue.emplace_front(std::move(item));
+
+				std::unique_lock<std::mutex> ul(muxBlocking);
+				cvBlocking.notify_one();
+			}
+
+			bool empty() {
+				std::scoped_lock lock(naxQueue);
+				return deqQueue.empty();
+			}
+
+			size_t count() {
+				std::scoped_lock lock(maxQueue);
+				return deqQueue.size();
+			}
+
+			void clear() {
+				std::scoped_lock lock(maxQueue);
+				deqQueue.clear();
+			}
+
+			void wait() {
+				while (empty()) {
+					std::unique_lock<std::mutex> ul(muxBlocking);
+					cvBlocking.wait(ul);
+				}
+			}
+
+			protected:
+				std::mutex muxQueue;
+				std::deque<T> deqQueue;
+				std::condition_variable cvBlocking;
+				std::mutex muxBlocking;
 		};
 	}
 
